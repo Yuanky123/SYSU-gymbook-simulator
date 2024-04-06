fc68f03c8bb8b0922a3b47111725a3e0eceefc84
Kangyu Yuan <85436662+Yuanky123@users.noreply.github.com>
2024-04-06 21:25:44 +0800

    1

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import 'normalize.css'
-import 'reset-css'
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-
-createApp(App).use(store).use(router).mount('#app')
